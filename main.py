c41d79a7f7e1a9b7831bf18574a2dd6bb942f462
ahamednadeem02 <111524475+ahamednadeem02@users.noreply.github.com>
2023-04-10 17:40:04 +0530

    last

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ df = pd.read_csv("topics (1).csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
-    # Set the header
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     for y in range(20, 298, 10):
         pdf.line(10, y, 200, y)
 
-    # Set the footer
+
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
